eb8af36d3994bf5f96aeb56e8520dd60045703bc
Khamin Gleb <nimik19@yandex.ru>
2024-06-05 18:10:49 +0300

    Реализован Таб бар. Стилизован

---
+++ ImageFeed2/ProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed2
+//
+//  Created by Глеб Хамин on 05.06.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+}
+
